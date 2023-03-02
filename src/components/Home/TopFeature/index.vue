1ea98f0df2160d01ff619ff13b3878bb05d35134
tolie <ben37839@gmail.com>
2023-03-02 21:43:05 +0800

    🔧️add:connected to mongoDB;❎️remove:postcss-pxrem

--- src/components/Home/TopFeature/index.vue
+++ src/components/Home/TopFeature/index.vue
@@ -1,56 +1,52 @@
 <!-- author: TOLIE -->
 <!-- date:  -->
 <!-- description:  -->
-<script setup lang="ts">
-import ArticleCard from "../../Slots/ArticleCard/index.vue";
-defineProps<{ title: string }>();
+<script setup lang='ts'>
+import ArticleCard from '../../Slots/ArticleCard/index.vue'
+defineProps<{ title: string }>()
+
 </script>
 
 <template>
   <ArticleCard :title="title">
-    <el-row
-      :gutter="20"
-      class="top-feature-list"
-    >
+    <el-row :gutter="20" justify="space-between" align="middle" class="top-feature-list">
       <el-col :span="6">
         <div class="grid-content ep-bg-purple">
-          <!-- <img src="@/assets/兔耳.jpg" alt="background" /> -->
+          <img src="@/assets/兔耳.jpg" alt="background">
         </div>
       </el-col>
       <el-col :span="6">
         <div class="grid-content ep-bg-purple">
-          <!-- <img src="@/assets/兔耳.jpg" alt="background" /> -->
+          <img src="@/assets/兔耳.jpg" alt="background">
         </div>
       </el-col>
       <el-col :span="6">
         <div class="grid-content ep-bg-purple">
-          <!-- <img src="@/assets/兔耳.jpg" alt="background" /> -->
+          <img src="@/assets/兔耳.jpg" alt="background">
         </div>
       </el-col>
     </el-row>
   </ArticleCard>
 </template>
 
-<style scoped lang="less">
+
+<style scoped lang='less'>
 .top-feature-list {
-  width: 750px;
-  height: 160px;
+  width: 100%;
+  height: 180px;
   margin: 0 !important;
 
   & :deep(.el-col) {
-    min-width: calc(710px / 3);
+    max-width: 33.3%;
     height: 100%;
   }
 }
 
 .grid-content {
   height: 100%;
-  width: 100%;
-  background: url(@/assets/兔耳.jpg) no-repeat center;
-  background-size: contain;
-  // img {
-  //   height: 100%;
-  //   // width: 100%;
-  // }
+  img {
+    height: 100%;
+    // width: 100%;
+  }
 }
 </style>
